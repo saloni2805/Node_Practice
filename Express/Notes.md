d432d24a242381fca645079d3f0d65dfd7560e46
Saloni Potdar <salonispotdar@gmail.com>
2025-04-08 11:53:53 +0530

    `Added EJS templating engine and updated dependencies`

---
+++ Express/Notes.md
@@ -0,0 +1,14 @@
+Logical Code
+
+<% EJS syntax to write Logical code %>
+
+<%= As a text display data %>
+
+<%- As a html display data %>
+
+<!-- One EJS inside another -->
+
+<%-include('external.ejs') %>
+
+<!-- Data Travesing to EJS -->
+<!-- When we are passing dtata to EJS there must be passed through object {} -->
--- Express/index.js
+++ Express/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 
+// Middleware to serve static files like images
+// Built in express middleware
+app.use(express.static("public/"))
+
 const menu = `
 <ul>
 <li><a href="/">Home</a></li>
@@ -10,22 +14,35 @@ const menu = `
     `
 // Route for home page
 app.get("/", (req, res) => {
-  res.send(`${menu}</br> <h1>Home Page</h1>`)
+  // res.send(`${menu}</br> <h1>Home Page</h1>`)
+  res.render("home.ejs") //  ---> Server-Side data rendering <---
 })
 
 // Route for about Page
 app.get("/about", (req, res) => {
   const arr = ["Saloni", "Sayani", "Pooja"]
   res.send(arr)
+  // const obj = {
+  //   name: "saloni",
+  //   contact: 6767676767,
+  // }
+  // res.send(obj)
 })
 
 // Route for contact Page
 app.get("/contact", (req, res) => {
-  const obj = {
-    name: "saloni",
-    contact: 6767676767,
+  //  Data Travesing to EJS
+  //  When we are passing dtata to EJS there must be passed through object {}
+
+  // 'obj' acts as a envelop
+  // cover letter
+  const user = {
+    name: "Saloni",
+    age: 23,
+    id: 34,
   }
-  res.send(obj)
+  const obj = { data: user }
+  res.render("contact.ejs", obj) //  ---> Server-Side data rendering
 })
 
 const PORT = 3000
